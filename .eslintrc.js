511671d45234053b6f2ef6f42fe058b696a99a71
Dhemy <imdhemy@gmail.com>
2023-06-21 23:15:28 +0200

    [dev] Bootstrap the project (#1)
    * [dev] install typscript
    
    * [dev] install required dependencies
    
    * [dev] create styorybook dir
    
    * [dev] config ts
    
    * [dev] config eslint
    
    * [dev] config prettier
    
    * [dev] update scripts
    
    * [dev] add babel presets
    
    * [component] create default component
    
    * [dev] add component examples

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'import', 'jsx-a11y'],
+  extends: ['eslint:recommended', 'google', 'plugin:react/recommended', 'plugin:storybook/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2022,
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  rules: {
+    // Add any additional rules or overrides here
+  }
+};
